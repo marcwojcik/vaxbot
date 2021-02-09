e6dd488261e4f7b5e9c7501d43cdb2c8a288e4ee
Marc Wojcik <marcwojcik@gmail.com>
2021-02-09 18:02:11 -0500

    Update pipinstall.bash

--- pipinstall.bash
+++ pipinstall.bash
@@ -1,6 +1,2 @@
 #!/bin/bash
-python3 -m pip install --user boto3
-python3 -m pip install --user bs4
-python3 -m pip install --user tweepy
-python3 -m pip install --user pyyaml
-python3 -m pip install --user selenium
+pip3 install --user -r requirements.txt
