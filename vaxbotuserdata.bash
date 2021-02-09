2a8a0af0a993d633518bb5f91cb02bc96c45775e
Marc Wojcik <marcwojcik@gmail.com>
2021-02-09 16:23:00 -0500

    Update vaxbotuserdata.bash

--- vaxbotuserdata.bash
+++ vaxbotuserdata.bash
@@ -1,16 +1,16 @@
 #!/bin/bash
 mkdir /home/ec2-user/vaxbot
-cd vaxbot
+chown -R ec2-user:ec2-user /home/ec2-user/vaxbot
 yum update -y
 yum install git -y
-git clone https://github.com/marcwojcik/vaxbot.git
+git clone https://github.com/marcwojcik/vaxbot.git /home/ec2-user/vaxbot
 
 yum install python3 -y
 
 cd /tmp/
 wget https://chromedriver.storage.googleapis.com/2.37/chromedriver_linux64.zip
 unzip chromedriver_linux64.zip
-sudo mv chromedriver /usr/bin/chromedriver
+mv chromedriver /usr/bin/chromedriver
 
 curl https://intoli.com/install-google-chrome.sh | bash
-sudo mv /usr/bin/google-chrome-stable /usr/bin/google-chrome
+mv /usr/bin/google-chrome-stable /usr/bin/google-chrome
