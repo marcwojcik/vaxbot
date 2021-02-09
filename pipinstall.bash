485f0c62a1982f50fd06c614768c379c350b101d
Marc Wojcik <marcwojcik@gmail.com>
2021-02-09 16:19:21 -0500

    Create pipinstall.bash

---
+++ pipinstall.bash
@@ -0,0 +1,6 @@
+#!/bin/bash
+python3 -m pip install --user boto3
+python3 -m pip install --user bs4
+python3 -m pip install --user tweepy
+python3 -m pip install --user pyyaml
+python3 -m pip install --user selenium
