ecafedc9623185368991f4f6c961e681d0c16d0e
Marc Wojcik <marcwojcik@gmail.com>
2021-02-09 16:18:37 -0500

    Update vaxbotuserdata.bash

--- vaxbotuserdata.bash
+++ vaxbotuserdata.bash
@@ -1,16 +1,11 @@
 #!/bin/bash
-mkdir /home/ec2-user/projects
-cd projects
+mkdir /home/ec2-user/vaxbot
+cd vaxbot
 yum update -y
 yum install git -y
 git clone https://github.com/marcwojcik/vaxbot.git
 
-sudo yum install python3 -y
-exit
-python3 -m pip install --user boto3
-python3 -m pip install --user bs4
-python3 -m pip install --user tweepy
-python3 -m pip install --user pyyaml
+yum install python3 -y
 
 cd /tmp/
 wget https://chromedriver.storage.googleapis.com/2.37/chromedriver_linux64.zip
@@ -19,5 +14,3 @@ sudo mv chromedriver /usr/bin/chromedriver
 
 curl https://intoli.com/install-google-chrome.sh | bash
 sudo mv /usr/bin/google-chrome-stable /usr/bin/google-chrome
-
-python3 -m pip install --user selenium
