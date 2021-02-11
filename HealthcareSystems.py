23e09e8c1e9162ce16760d7aba082a8fb2d2f150
Marc Wojcik <marcwojcik@gmail.com>
2021-02-11 16:30:32 -0500

    Update HealthcareSystems.py
    Added CentraState

--- HealthcareSystems.py
+++ HealthcareSystems.py
@@ -13,4 +13,16 @@ class Hackensack:
             return True
         else:
             return False
+        
+class CentraState:
+
+    def check_vaccines(self):
+        options = Options()
+        options.headless = True
+        driver = webdriver.Chrome(options=options)
+        driver.get("https://centrastatevac.wpengine.com/vaccine-update/")
+        if not driver.page_source.__contains__("Our COVID-19 Vaccine Request form has been closed"):
+            return True
+        else:
+            return False
 
